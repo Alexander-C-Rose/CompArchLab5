418ec469d048b41c26519486bdb151fa4e44cbce
Alexander-C-Rose <77861295+Alexander-C-Rose@users.noreply.github.com>
2021-04-23 16:13:23 -0500

    Update fsm.sv

--- fsm/fsm.sv
+++ fsm/fsm.sv
@@ -6,15 +6,15 @@ module fsm(Z, X, clk, reset);
    
    output logic Z;
 
-   parameter [3:0] S0 = 4'd0,
-     S1 = 4'd1,
-     S2 = 4'd2,
-     S3 = 4'd3,
-     S4 = 4'd4,
-     S5 = 4'd5,
-     S6 = 4'd6,
-     S7 = 4'd7,
-     Idle = 4'd8;
+   parameter [3:0]  S0 = 4'd0,
+					S1 = 4'd1,
+					S2 = 4'd2,
+					S3 = 4'd3,
+					S4 = 4'd4,
+					S5 = 4'd5,
+					S6 = 4'd6,
+					S7 = 4'd7,
+					Idle = 4'd8;
 
    logic [3:0] CURRENT_STATE;
    logic [3:0] NEXT_STATE;
